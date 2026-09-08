4c28e52d5330cecd09083e12b0b30a60306ce2d6
Студенты Черных Д.К. <chedk@teach.local>
2026-09-08 11:56:51 +0300

    feat: init mobile web app with actions

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.helloapp',
+  appName: 'Hello App',
+  webDir: 'dist'
+};
+
+export default config;
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+const app = document.getElementById('app');
+if (app) {
+    app.innerHTML = `<h1>Hello World из TypeScript!</h1><p>Собрано через GitHub Actions 🚀</p>`;
+}
